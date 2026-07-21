8244a05b60a587db5dfc6275e7676a09533bf8e6
Divyanshu <dvnsh76@gmail.com>
2026-07-21 11:25:19 +0530

    commit

--- Python/main.py
+++ Python/main.py
@@ -17,4 +17,31 @@ from tkinter import Variable
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
 
 
-# Comments and Variable
+                    #1. Comments and Variable
+
+# This line below is used for Printing
+print("Hello NYC")
+
+# Variable
+
+anything = 12312
+print(anything)
+
+any_thing = 123123123
+print(any_thing)
+
+
+# ( anything can become a variable )
+''' But, There are some rules
+RULES - THESE WILL CAUSE ERRORS:
+
+1name = "x" --> can't start with a number
+my name = "x" --> no space are allowed
+my-name = "x" --> no special characters are allowed (except underscore _ )  '''
+
+""" Naming Conventions ( Correct Way )
+
+camelCase -> myVariableName
+PascalCase -> MyVariableName
+snake_case -> my_variable_name    # Python prefers this
+"""
